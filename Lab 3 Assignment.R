1689dcdd87ad307df3fa16a816f1b217f8808660
Your Name <you@example.com>
2021-02-02 11:15:23 -0600

    Update

--- Lab 3 Assignment.R
+++ Lab 3 Assignment.R
@@ -10,34 +10,32 @@ birds <- ebird_taxonomy %>%           # start with the ebird_taxonomy data
   as_tibble() %>%                     # tibbles print better in the console
   filter(category == "species")       # remove non-species taxa
 
-tiger_data <- read_csv("https://whitlockschluter.zoology.ubc.ca/wp-content/data/chapter02/chap02e2aDeathsFromTigers.csv")
+birds
 
-tiger_data
+distinct(birds, order)
 
-distinct(tiger_data, activity)
+count(birds, family_common)
 
-count(tiger_data, activity)
+ggplot(data = birds)
 
-ggplot(data = tiger_data)
+ggplot(data = birds) +
+  geom_bar(mapping = aes(x = order))
 
-ggplot(data = tiger_data) +
-  geom_bar(mapping = aes(x = activity))
+ggplot(data = birds) +
+  geom_bar(mapping = aes(x = fct_infreq(order)))
 
-ggplot(data = tiger_data) +
-  geom_bar(mapping = aes(x = fct_infreq(activity)))
+ggplot(data = birds) +
+  geom_bar(mapping = aes(x = fct_infreq(order)), fill = "#C5351B")
 
-ggplot(data = tiger_data) +
-  geom_bar(mapping = aes(x = fct_infreq(activity)), fill = "#C5351B")
+ggplot(data = birds) +
+  geom_bar(mapping = aes(x = fct_infreq(order)), fill = "#C5351B") +
+  labs(x = "Order", y = "Frequency (number of birds)")
 
-ggplot(data = tiger_data) +
-  geom_bar(mapping = aes(x = fct_infreq(activity)), fill = "#C5351B") +
-  labs(x = "Activity", y = "Frequency (number of people)")
-
-ggplot(data = tiger_data) +
-  geom_bar(mapping = aes(x = fct_infreq(activity)), fill = "#C5351B", 
+ggplot(data = birds) +
+  geom_bar(mapping = aes(x = fct_infreq(order)), fill = "#C5351B", 
            width = .8) +
-  labs(x = "Activity", y = "Frequency (number of people)") +
-  scale_y_continuous(limits = c(0, 50), expand = expansion(mult = 0)) +
+  labs(x = "Order", y = "Frequency (number of birds)") +
+  scale_y_continuous(limits = c(0, 7000), expand = expansion(mult = 0)) +
   theme_classic(base_size = 12) +
   theme(
     axis.title = element_text(face = "bold"),
--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 Lab 3: Introduction to ggplot
 ================
 Tyler Edvall
-2021-01-28
+2021-02-02
 
 Welcome to my Lab 2 repository for Quantitative Biology BIOL 275 at
 MSUM.
@@ -10,8 +10,33 @@ MSUM.
 
 The objectives of this lab are:
 
-  - Use the layered grammar of graphics
-  - Create a graph showing the distribution of a single variable
+Use the layered grammar of graphics:
+
+``` 
+ -Create a blank graph using the ggplot() function
+
+ -Add geometric objects to a graph
+
+ -Define an aesthetic mapping for a graph
+
+ -Add labels to a graph
+
+ -Fix some common styling
+```
+
+Create a graph showing the distribution of a single variable:
+
+``` 
+ -Create a bar chart for a categorical variable using the geom_col() and geom_bar() functions
+
+ -Arrange the categories in a bar chart by frequency
+
+ -Create a histogram for a numerical variable using the geom_histogram() function
+
+ -Choose and set appropriate histogram bin widths
+
+ -Set the image width for an R Markdown code chunk
+```
 
 ## Repository contents
 
